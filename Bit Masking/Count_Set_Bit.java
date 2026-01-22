2d072967c8b6936d8fd4bfefe8a8863899a9048b
Rohit Gupta <rohitgupta1006@gmail.com>
2026-01-22 15:54:14 +0530

    Refactor Count_Set_Bit and Magic_number classes to improve code structure and readability

--- Bit Masking/Count_Set_Bit.java
+++ Bit Masking/Count_Set_Bit.java
@@ -1,19 +1,27 @@
 public class Count_Set_Bit {
-  public static void main(String[] args) {
 
-  }
-  public static int Count_SetBit(int n)
-  {
-    int ans=0;
-    while(n>0)
-    {
-      if((n&1) !=0)
-      {
-        ans++;
-      }
-      n >>=1;  // n = n>>1
-    }
-    return ans;
-  }
-  
+	public static void main(String[] args) {
+		int n = 84;
+
+	}
+
+	public static int Fast_Count_SetBit(int n) {
+		int ans = 0;
+		while (n > 0) {
+			ans++;
+			n = n & (n - 1);
+		}
+		return ans;
+	}
+
+	public static int Count_SetBit(int n) {
+		int ans = 0;
+		while (n > 0) {
+			if ((n & 1) != 0) {
+				ans++;
+			}
+			n >>= 1;// n=n>>1;
+		}
+		return ans;
+	}
 }
--- Bit Masking/Magic_number.java
+++ Bit Masking/Magic_number.java
@@ -1,8 +1,22 @@
 public class Magic_number {
-  public static void main(String[] args)
-  {
 
-  }
- 
-  
+	public static void main(String[] args) {
+		int n = 9;
+		System.out.println(Nthnumber(n));
+
+	}
+
+	public static int Nthnumber(int n) {
+		int mul = 5;
+		int ans = 0;
+		while (n > 0) {
+			if ((n & 1) != 0) {
+				ans += mul;
+			}
+			n >>= 1;
+			mul = mul * 5;
+		}
+		return ans;
+	}
+
 }
