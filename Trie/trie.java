d6ce201ee7f5daa1e54607f1d5de23e3a0cfcde9
Rohit Gupta <rohitgupta1006@gmail.com>
2026-02-04 12:26:46 +0530

    leetcode 208

--- Trie/trie.java
+++ Trie/trie.java
@@ -34,5 +34,37 @@ public class trie {
     }
     curr.isterminal=true;
   }
+  public boolean search(String word)
+  {
+    Node curr=root;
+    for(int i = 0; i<word.length();i++)
+    {
+      char ch=word.charAt(i);
+      if(curr.child.containsKey(ch))
+      {
+        curr=curr.child.get(ch);
+      }
+      else{
+        return false;
+      }
+    }
+    return curr.isterminal;
+  }
+  public boolean startsWith(String prefix)
+  {
+    Node curr=root;
+    for(int i = 0; i<prefix.length();i++)
+    {
+      char ch=prefix.charAt(i);
+      if(curr.child.containsKey(ch))
+      {
+        curr=curr.child.get(ch);
+      }
+      else{
+        return false;
+      }
+    }
+    return true;
+  }
   
 }
