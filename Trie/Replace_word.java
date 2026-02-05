968fafa7d3939ac2599b23953f2389280a3021de
Rohit Gupta <rohitgupta1006@gmail.com>
2026-02-05 12:28:17 +0530

    leetcode 648

---
+++ Trie/Replace_word.java
@@ -0,0 +1,83 @@
+package Trie;
+import java.util.*;
+
+public class Replace_word {
+
+    public static void main(String[] args)
+    {
+       String[] Dictionary={"cat","bat","rat"}; 
+       Trie t=new Trie(); 
+       for(String s:Dictionary) 
+        { 
+          t.insert(s); 
+
+        } 
+        String sentence="the cattle was rattled by the battery"; 
+        String[] arr=sentence.split(" ");
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<arr.length;i++)
+        {
+          String get = t.search(arr[i]);
+            sb.append(get+" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+
+    static class Trie {
+
+        class Node{
+            char ch;
+            String isterminal;
+            HashMap<Character,Node> child;
+
+            public Node(char ch){
+                this.ch = ch;
+                child = new HashMap<>();
+            }
+        }
+
+        private Node root = new Node('*');
+
+        public void insert(String word)
+        {
+          Node curr=root;
+          for(int i = 0; i<word.length();i++)
+          {
+            char ch=word.charAt(i);
+            if(curr.child.containsKey(ch))
+            {
+              curr=curr.child.get(ch);
+            }
+            else{
+              Node nn=new Node(ch);
+              curr.child.put(ch,nn);
+              curr=nn;
+            }
+          }
+          curr.isterminal=word;
+        }
+
+        // 🔥 Important method for replace word problem
+        public String search(String word)
+        {
+            Node curr = root;
+            for(int i=0;i<word.length();i++)
+            {
+                char ch = word.charAt(i);
+                if(curr.child.containsKey(ch))
+                {
+                    curr = curr.child.get(ch);
+                    if(curr.isterminal != null)
+                    {
+                        return curr.isterminal;
+                    }
+                }
+                else
+                {
+                    return word;
+                }
+             }
+             return word;
+        }
+    }
+}
